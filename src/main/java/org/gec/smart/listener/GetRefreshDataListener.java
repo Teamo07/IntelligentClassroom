f536bd29fdcea83c72d093b2cffb13a848f67e8c
KarlKris <953591544@qq.com>
2018-11-27 16:32:11 +0800

    11/27 添加后台模块以及其ssm相关依赖

--- src/main/java/org/gec/smart/listener/GetRefreshDataListener.java
+++ src/main/java/org/gec/smart/listener/GetRefreshDataListener.java
@@ -9,14 +9,15 @@ import org.gec.smart.task.RefreshTask;
 
 public class GetRefreshDataListener implements ServletContextListener {
 
-	public void contextInitialized(ServletContextEvent arg0)  { 
+	@Override
+    public void contextInitialized(ServletContextEvent arg0)  {
         System.out.println("容器启动了...");
         //启动定时任务(每个30秒钟自动执行定时任务一次)
         new Timer().schedule(new RefreshTask(), 0, 1000 * 30);
    }
 
-   public void contextDestroyed(ServletContextEvent arg0)  { 
-
+   @Override
+   public void contextDestroyed(ServletContextEvent arg0)  {
 	    System.out.println("容器关闭了...");
    }
 	

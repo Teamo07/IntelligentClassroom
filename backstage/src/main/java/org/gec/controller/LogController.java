b8919e085f663576a59c1a27fec3c0d0260531a1
wuyeshen <1014452044@qq.com>
2018-12-04 17:14:24 +0800

    12 04  空调控制日志完成

--- backstage/src/main/java/org/gec/controller/LogController.java
+++ backstage/src/main/java/org/gec/controller/LogController.java
@@ -1,7 +1,38 @@
 package org.gec.controller;
 
+import org.gec.model.AirConditionerLog;
+import org.gec.model.PageModel;
+import org.gec.service.IAirConditionerLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * @author:
  **/
-public class LogController {
+
+@RestController
+@RequestMapping("/admin")
+public class LogController  extends BaseController{
+
+
+        @Autowired
+        private IAirConditionerLogService airConditionerLogService;
+
+        @RequestMapping(path="/getAirConditionerLog.do", produces="application/json;charset=utf-8")
+        @ResponseBody
+        public PageModel getAirConditionerLog(Integer page, Integer rows) {
+            int total = airConditionerLogService.getTotal();
+            List<AirConditionerLog> data = airConditionerLogService.findAir(page, rows);
+            PageModel pageModel = new PageModel();
+            pageModel.setTotal(total);
+            pageModel.setRows(data);
+            return pageModel;
+        }
+
+
+
 }

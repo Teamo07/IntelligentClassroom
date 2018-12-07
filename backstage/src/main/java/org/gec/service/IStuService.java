1f4f791ca07c05ae09e30f8ab00d4429e038999f
wuyeshen <1014452044@qq.com>
2018-12-07 20:29:51 +0800

    2018.12.07.20：24李定国， 加入学生导入导出，设备导入导出功能 修复命名bug

--- backstage/src/main/java/org/gec/service/IStuService.java
+++ backstage/src/main/java/org/gec/service/IStuService.java
@@ -4,6 +4,8 @@ import org.gec.model.Asset;
 import org.gec.model.PageModel;
 import org.gec.model.Student;
 
+import java.util.List;
+
 /**
  * @Author LZM
  * @Date 2018/12/6 14:44
@@ -19,4 +21,12 @@ public interface IStuService {
     void updateStu(Student student);
 
     void deleteStu(String id);
+
+    List<Student> findStu();
+
+    /**
+     批量添加学生
+     @param students
+     */
+    void addStudent(List<Student> students);
 }
